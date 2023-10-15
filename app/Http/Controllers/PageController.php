f1df09b81ff27b4287599a3f029dc2d899892254
plastikSun <marcom2k01@gmail.com>
2023-10-15 12:42:10 +0200

    connected to db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
 
-    function homepage()
+    public function homepage()
     {
+        $movies = Movie::all(); //prendo tutti i film dal db
+        dd($movies);
         return view('home');
     }
 }

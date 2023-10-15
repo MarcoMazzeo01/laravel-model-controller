f5d470eb8319cd56c4fda8b42e2b03feb0b0c97e
plastikSun <marcom2k01@gmail.com>
2023-10-15 11:58:24 +0200

    add: homepage, app layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('page-title', 'Home')
+
+@section('main-content')
+
+    <h1>hi:3</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Model Controller - @yield('page-title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            @yield('main-content')
+        </div>
+    </main>
+</body>
+
+</html>
